d11449668cf7cffa78383287bfa42ca39c12185d
Zeki Unal <zekiunal@gmail.com>
2025-04-01 19:09:31 +0300

    First commit

---
+++ src/Infrastructure/Datasource/Library/RouterDatasource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Nexus\Infrastructure\Datasource\Library;
+
+use Nexus\Domain\Datasource\RouterDatasourceInterface;
+use Nexus\Router;
+
+class RouterDatasource implements RouterDatasourceInterface
+{
+    private array $routes;
+
+    public function __construct(array $routes)
+    {
+        $this->routes = $routes;
+    }
+    public function handleMessage(array $message): array
+    {
+        $method = $message['method'] ?? 'GET';
+        $uri = $message['path'] ?? '/';
+        $data = $message['data'] ?? [];
+        $router = new Router($this->routes);
+        $response = $router->dispatch($method, $uri, $data);
+        unset($router);
+        return $response;
+    }
+}
